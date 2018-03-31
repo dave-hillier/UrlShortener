43e9e5fed00b63dbeff910af179ebeaa3126d65f
Dave Hillier <dave@hillier.co>
2018-03-31 22:09:26 +0100

    Initial commit

---
+++ UrlShortener.Tests/UrlShortenerIntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace UrlShortener.Tests
+{
+  public class UrlShortenerIntegrationTests
+  {
+    private readonly TestServer _server;
+    private readonly HttpClient _client;
+
+    public UrlShortenerIntegrationTests()
+    {
+      _server = new TestServer(new WebHostBuilder()
+        .UseEnvironment("Test")
+        .UseStartup<Startup>());
+      _client = _server.CreateClient();
+    }
+
+    [Fact]
+    public async Task ReturnHelloWorld()
+    {
+      var response = await _client.GetAsync("/");
+      response.EnsureSuccessStatusCode();
+
+      var responseString = await response.Content.ReadAsStringAsync();
+
+      Assert.Equal("Hello World!", responseString);
+    }
+
+    [Fact]
+    public async Task PostLongUrl()
+    {
+      var response = await _client.GetAsync("/");
+      response.EnsureSuccessStatusCode();
+
+      var responseString = await response.Content.ReadAsStringAsync();
+
+      Assert.Equal("Hello World!", responseString);
+    }
+  }
+}
---
+++ UrlShortener/Controllers/ShortenerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortener.Controllers
+{
+  [Route("/")]
+  public class ShortenerController : Controller
+  {
+    [HttpGet("{shortUrl}")]
+    public IActionResult Get(string shortUrl)
+    {
+      try
+      {
+        var decodedTxt = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(shortUrl));
+        return new RedirectResult(decodedTxt);
+      }
+      catch (FormatException)
+      {
+        return new NotFoundResult();
+      }
+    }
+
+    // TODO: authorize
+    [HttpPost]
+    public IActionResult Post([FromQuery]string longUrl)
+    {
+      string v = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(longUrl));
+      string url = Url.Action("Get", new { s = v });
+      return new OkObjectResult(new { Long = longUrl, Short = url });
+    }
+
+  }
+}
